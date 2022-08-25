5917f4f31deb6204acdc75ea1657b27dad267cb4
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-25 07:15:07 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Whats the current temperature?");
+
+            float currentTemp;
+            bool sucess =float.TryParse(Console.ReadLine(), out currentTemp );
+
+            if (!sucess)
+            {
+                Console.WriteLine("Not a valid Temperature");
+            }
+            else
+            {
+                tempCheck(currentTemp);
+            }
+
+            
+        }
+        public static void tempCheck(float currentTemp)
+        {
+            string output = currentTemp < 16 ? "it is too cold here" : currentTemp >= 16 && currentTemp <= 28 ? "it is ok" : "it is hot here";
+
+            Console.WriteLine(output);
+            Console.ReadLine();
+        }
+    }
+}
